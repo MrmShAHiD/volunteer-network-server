228840104e42fc1f7668eaa8e75bf845f8a7c24d
SHAHID <43965492+MrmShAHiD@users.noreply.github.com>
2020-10-06 15:06:48 +0600

    problem fixed

--- index.js
+++ index.js
@@ -2,70 +2,87 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const objectId = require('mongodb').ObjectID;
-
+require('dotenv').config()
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zeypn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zeypn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 
+// root 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('database working')
 })
 
+// mongodb
 client.connect(err => {
   const activityCollection = client.db("volunteerActivity").collection("activity");
   const registerListCollection = client.db("volunteerActivity").collection("registerList");
-
-  console.log('database connected successfully')
   
-//   Create post 
   app.post("/addVolunteerActivity", (req, res) => {
-      const volunteerActivity = req.body;
-      activityCollection.insertOne(volunteerActivity)
-      .then(result => {
-          res.send(res.insertedCount)
-      })
+    const volunteerActivity = req.body;
+    activityCollection.insertOne(volunteerActivity)
+    .then(result => {
+      res.send(result.insertedCount)
+    })
   })
-
-//   get data from database
+  
   app.get('/volunteerActivity', (req, res) => {
-      activityCollection.find({})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
+    activityCollection.find({})
+    .toArray((err, documents) =>{
+      res.send(documents)
+    })
   })
 
+
+  // register to database
   app.post('/addRegister', (req, res) => {
-      const register = req.body;
-      registerListCollection.insertOne(register)
-      .then(result => {
-          res.send(result.insertedCount)
-      })
+    const register = req.body;
+    registerListCollection.insertOne(register)
+    .then(result => {
+      res.send(result.insertedCount)
+    })
   })
 
-//   get register list of volunteer
-  app.get('/registers/:email', (req, res) => {
-      const userEmail = req.params.email;
-      registerListCollection.find({email: userEmail})
-      .toArray((err, documents) => {
-          res.send(documents);
-      })
+  app.get('/registerList/:email', (req, res) => {
+    const userEmail = req.params.email;
+    registerListCollection.find({email:userEmail})
+    .toArray((err, documents) =>{
+      res.send(documents)
+    })
   })
 
-//   delete from database
+  // delete activity 
   app.delete('/delete/:id', (req, res) => {
     const id = req.params.id;
     registerListCollection.deleteOne({_id: objectId(req.params.id)})
-    .then((result) =>{
-        res.send(result.deletedCount)
+    .then(result => {
+      res.send(result.deletedCount)
+    })
+  })
+
+  // admin part read all user
+  app.get('/allRegisterList', (req, res) => {
+    registerListCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+  
+  // admin delete user
+  app.delete('/deleteUserRegister/:id', (req, res) => {
+    const id = req.params.id;
+    registerListCollection.deleteOne({_id: objectId(req.params.id)})
+    .then(result => {
+      res.send(result.deletedCount)
     })
   })
 
 });
 
+
 const port = 5000;
-app.listen(process.env.PORT || port)
+app.listen(port)
